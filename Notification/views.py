983933f92111ef150ec7022f5a12cf21ef67097c
Madhav Pruthi <madhavpruthi0@gmail.com>
2018-03-31 13:31:19 +0530

    adding some info

--- Notification/views.py
+++ Notification/views.py
@@ -1,11 +1,17 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User, Group
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from accounts.models import Faculty
 
+
+def is_member(user):
+    return user.groups.filter(name='Faculty').exists()
+
+@user_passes_test(is_member,login_url='/accounts/FacLogIn')
 @login_required(login_url='/accounts/FacLogIn')
 def FacHome(request , fac_id):
-
-    faculty = get_object_or_404(User , pk=fac_id)
-    print(faculty)
-    return HttpResponse("Hello There. I am Id: " + str(fac_id))
+    user = get_object_or_404(User , pk=fac_id)
+    object = Faculty.objects.get(user=user)
+    print(user.groups.all())
+    return render(request, 'Notification/FacHome.html' , { 'object': object , 'faculty':user})
